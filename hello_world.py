0e8dc7c61664c26d110a3bfe2f73b70b63f4dea5
gaolong <gaolong@36kr.com>
2019-03-26 19:26:59 +0800

    modified on mac

--- hello_world.py
+++ hello_world.py
@@ -1,28 +1,33 @@
 import os
 
-file = open('d:/glt/data.txt', 'w')
+# file = open('d:/glt/data.txt', 'w')
+#
+# file.write('Hello file world! \n')
+# file.write('Bye file world. \n')
+# file.write('Bye file world. \n')
+#
+# file.close()
+#
+# file = open('d:/glt/data.txt', 'r')
+# lines = file.readlines()
+# for line in lines:
+#     print(line, end='')
+# file.close()
 
-file.write('Hello file world! \n')
-file.write('Bye file world. \n')
-file.write('Bye file world. \n')
 
-file.close()
+targetDir = "/Users/pxcm-0101-01-0128/Downloads"
+# targetDir = 'd:/glt/data.txt'
 
-file = open('d:/glt/data.txt', 'r')
-lines = file.readlines()
-for line in lines:
-    print(line, end='')
-file.close()
+# file = open(targetDir)
+# for line in file:
+#     print(line, end='')
+# file.close()
 
-file = open('d:/glt/data.txt')
-for line in file:
-    print(line, end='')
-file.close()
+# file = open('d:/glt/data.txt', 'w', encoding='utf-8')
 
-file = open('d:/glt/data.txt', 'w', encoding='utf-8')
-for (dir, subdirs, files) in os.walk("D:\新建文件夹"):
-    print('[' + dir + ']')
-    file.write(dir + '\n')
+for (path, subdirs, files) in os.walk(targetDir):
+    print('[' + path + ']')
+    # file.write(dir + '\n')
     for fname in files:
-        print(os.path.join(dir, fname))
-        file.write(os.path.join(dir, fname) + '\n')
+        print(os.path.join(path, fname))
+        # file.write(os.path.join(dir, fname) + '\n')
