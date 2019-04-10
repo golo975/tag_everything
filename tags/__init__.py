a527b465a97935ed976031861fa99f141e4097c2
gaolong <gaolong@36kr.com>
2019-04-10 18:59:01 +0800

    package

---
+++ tags/__init__.py
@@ -0,0 +1,64 @@
+class TagNode(object):
+    def __init__(self, name, parent):
+        self.__name = name
+        self.__parent = parent
+
+    def get_parent(self):
+        return self.__parent
+
+    def get_name(self):
+        return self.__name
+
+
+class Tag(TagNode):
+
+    def __repr__(self) -> str:
+        """
+        1. 文档注释必须用双引号？？？为什么？？？
+        2. -> 这是什么语法？？
+        """
+        return self.get_name()
+
+    def __str__(self) -> str:
+        return self.get_name()
+
+    def __eq__(self, o: object) -> bool:
+        """
+        怎样重新equals方法？
+        重写之后会影响哪些行为？
+        """
+        return isinstance(o, Tag) and super().__eq__(o) and super().get_name().__eq__(o.get_name())
+
+
+tag_default = Tag("default", None)
+
+
+class Entity(object):
+    def __init__(self):
+        self.__tags = {tag_default}
+
+    def add_tag(self, tag):
+        self.__tags.add(tag)
+        if len(self.__tags) >= 2 and tag_default in self.__tags:
+            self.__tags.remove(tag_default)
+
+    def rm_tag(self, tag):
+        if tag in self.__tags:
+            self.__tags.remove(tag)
+            if not self.__tags:
+                self.add_tag(tag_default)
+
+    def get_tags(self):
+        return self.__tags
+
+
+class EntityTagRelation(object):
+    pass
+
+
+tag_test = Tag("default", None)
+entity = Entity()
+print(entity.get_tags())
+
+entity.add_tag(tag_test)
+print(entity.get_tags())
