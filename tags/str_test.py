26c644569d47ddc1f686454605c4cbab451deb3b
gaolong <gaolong@36kr.com>
2019-05-16 19:52:12 +0800

    ...

---
+++ tags/str_test.py
@@ -0,0 +1,14 @@
+#
+s = '白日依山尽，黄河入海流。欲穷千里目，更上一层楼。'
+print(s[0])
+print(s[-2])
+print(s[:5])
+print(s[:-6])
+print(len(s))
+print('白' in s)
+print(range(5))
+print(type(range(5)))
+print(list(range(5)))
+print(type(list(range(5))))
+for i in range(5):
+    print(i)
